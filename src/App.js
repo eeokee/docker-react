461ef80d91b6eff11fbd686aea7d0c93b7f5b1fe
chenzhiqiang <eeokee@gmail.com>
2019-02-18 17:16:19 +0800

    test feature

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello React
+            Hi React
           </a>
         </header>
       </div>
